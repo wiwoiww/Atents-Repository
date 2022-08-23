476a02397198545faa22159b640fc869a31e9a51
wiwoiww <wiwoiww2@gmail.com>
2022-08-23 13:21:29 +0900

    ㅇㅇ

---
+++ 02_Shooting/Assets/Move.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position += Vector3.left
+    }
+}
