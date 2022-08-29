f8ee58a958289b44f571981955b740ecaa868fd1
wiwoiww <wiwoiww2@gmail.com>
2022-08-29 17:54:57 +0900

    8.29수업

--- 02_Shooting/Assets/Scripts/Asteroid.cs
+++ 02_Shooting/Assets/Scripts/Asteroid.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Asteroid : MonoBehaviour
 {
     public float rotateSpeed = 360.0f;
+    public float speed = 1.0f;
     void Update()
     {
         //transform.rotation *= Quaternion.Euler(new(0, 0, 90));    // 계속 90도씩 회전
         //transform.rotation *= Quaternion.Euler(new(0, 0, rotateSpeed * Time.deltaTime));    // 1초에 360도씩 회전
         transform.Rotate(rotateSpeed * Time.deltaTime * Vector3.forward);   // Vector3.forward 축을 기준으로 1초에 rotateSpeed도씩 회전
+        transform.Translate(speed * Time.deltaTime * Vector3.left, Space.World);
 
     }
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawLine(transform.position, new(1, Random.Range(1, 0), 1));
+    //}
 }
 // 운석이 화면 오른쪽 바깥의 랜덤한 지점에서 생성되서 화면 왼쪽 바깥의 랜덤한 지점으로 이동한다.
 // - AsteroidSpawner 클래스를 생성한다.
---
+++ 02_Shooting/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject Asteroid;
+    public float interval = 0.5f;
+
+    float minY = -4.0f;
+    float maxY = 4.0f;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());    
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)     
+        {
+            GameObject obj = Instantiate(Asteroid, transform);  
+            obj.transform.Translate(0, Random.Range(minY, maxY), 0);  
+            yield return new WaitForSeconds(interval);  
+        }
+    }
+
+}
