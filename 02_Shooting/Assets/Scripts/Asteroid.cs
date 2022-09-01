cbd903f0ab11840290546362643d58c01eb283d1
wiwoiww <wiwoiww2@gmail.com>
2022-09-01 10:40:52 +0900

    9.1일 수업

--- 02_Shooting/Assets/Scripts/Asteroid.cs
+++ 02_Shooting/Assets/Scripts/Asteroid.cs
@@ -1,15 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    public float rotateSpeed = 360.0f;
-    public float moveSpeed = 3.0f;
-    public Vector3 direction = Vector3.left;
+    public float rotateSpeed = 360.0f;          // 회전 속도
+    public float moveSpeed = 3.0f;              // 이동 속도
+    public Vector3 direction = Vector3.left;    // 운석이 이동할 방향
     public int hitPoint = 3;
 
     private GameObject explosion;
+
+    SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        sprite.flipX = true;
+        sprite.flipY = false;
+
+        //int rand = Random.Range(0, 100) & 2;
+        ////if(rand == 0)
+        ////{
+        ////    sprite.flipX = true;
+        ////}
+        ////else
+        ////{
+        ////    sprite.flipX = false;
+        ////}
+        //sprite.flipX = (rand == 0);
+
+        int rand = Random.Range(0, 4);          // rand에다가 0(0b_00),1(0b_01),2(0b_10),3(0b_11) 중 하나의 숫자를 랜덤으로 준다.
+
+        //rand & 0b_01 : rand의 제일 오른쪽 비트가 0인지 1인지 확인하는 작업
+        //((rand & 0b_01) != 0) : rand의 제일 오른쪽 비트가 1이면 true, 0이면 false를 리턴
+        sprite.flipX = ((rand & 0b_01) != 0);
+
+        //rand & 0b_10 : rand의 제일 오른쪽에서 두번째 비트가 0인지 1인지 확인하는 작업
+        //((rand & 0b_10) != 0) : rand의 제일 오른쪽에서 두번째 비트가 1이면 true, 0이면 false를 리턴
+        sprite.flipY = ((rand & 0b_10) != 0);
+
+        moveSpeed = Random.Range(2.0f, 4.0f);
+        rotateSpeed = Random.Range(30.0f, 360.0f);
+
+    }
 
     private void Start()
     {
