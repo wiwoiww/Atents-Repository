c5c390c115e1b4d02940f0fe1151f997365f9124
wiwoiww <wiwoiww2@gmail.com>
2022-09-04 20:25:08 +0900

    과제

--- 02_Shooting/Assets/Scripts/PowerUp.cs
+++ 02_Shooting/Assets/Scripts/PowerUp.cs
@@ -6,16 +6,12 @@ public class PowerUp : MonoBehaviour
 {
     float lifeTime = 10.0f;
 
-    private void Awake()
+    private void Start()
     {
-        lifeTime = 10.0f;
-        SelfCrush();
+        Destroy(this.gameObject, lifeTime);
     }
 
-    IEnumerator SelfCrush()
-    {
-        yield return new WaitForSeconds(lifeTime);
-
-        Destroy(this.gameObject);
-    }
 }
+// PowerUp 아이템은 1초에 한번씩 움직일 방향을 랜덤으로 결정한다.
+// PowerUp 아이템 Border에 부딪치면 반사된다.
+// PowerUp 아이템은 10초 후에 사라진다.
