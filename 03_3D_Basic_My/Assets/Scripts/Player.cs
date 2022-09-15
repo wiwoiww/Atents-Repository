24918f6cbf7dea4d3e70c60a3b6431df087c627c
wiwoiww <wiwoiww2@gmail.com>
2022-09-15 22:30:35 +0900

    9.15수업

--- 03_3D_Basic_My/Assets/Scripts/Player.cs
+++ 03_3D_Basic_My/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,6 +9,7 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float rotateSpeed = 180.0f;
+    public float jumpPower = 3.0f;
 
     float moveDir = 0.0f;
     float rotateDir = 0.0f;
@@ -26,11 +28,13 @@ public class Player : MonoBehaviour
         inputActions.Player.Enable();               // Player 액션맵에 들어있는 액션들을 처리하겠다.
         inputActions.Player.Move.performed += OnMoveInput; // Move 액션에 연결된 키가 눌러졌을 때 실행되는 함수를 연결(바인딩)
         inputActions.Player.Move.canceled += OnMoveInput;
+        inputActions.Player.Jump.performed += OnJumpInput;
 
     }
 
     private void OnDisable()
     {
+        inputActions.Player.Jump.performed -= OnJumpInput;
         inputActions.Player.Move.canceled -= OnMoveInput;
         inputActions.Player.Move.performed -= OnMoveInput; // 바인딩 해제
         inputActions.Player.Disable();              // Player 액션맵에 있는 액션들은 처리하지 않겠다.
@@ -51,15 +55,26 @@ public class Player : MonoBehaviour
         moveDir = input.y;      // w : +1, s : -1     전진인지 후진인지 결정
         rotateDir = input.x;    // a : -1, d : +1     좌회전인지 우회전인지 결정
     }
+    private void OnJumpInput(InputAction.CallbackContext _)
+    {
+        // 플레이어의 위쪽 방향(Up)으로 jumpPower만큼 즉시 힘을 추가한다.(질량 영향있음)
+        rigid.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+    }
 
     void Move()
     {
         ///rigid.MovePosition(transform.position +  moveSpeed * Time.fixedDeltaTime * dir);
-        rigid.MovePosition(rigid.position + moveSpeed * Time.fixedDeltaTime * moveDir * transform.forward );
+        rigid.MovePosition(rigid.position + moveSpeed * Time.fixedDeltaTime * moveDir * transform.forward );//리지드바디로 이동 설정
     }
 
     void Rotate()
     {
+        // 리지드바디로 회전 설정
+        rigid.MoveRotation(rigid.rotation * Quaternion.AngleAxis(rotateDir * rotateSpeed * Time.fixedDeltaTime, transform.up));
+
+        // Quaternion.Euler(0,rotateDir * rotateSpeed * Time.fixedDeltaTime,0)    // x,z축은 회전 없고 y축 기준으로 회전
+        // Quaternion.AngleAxis(rotateDir * rotateSpeed * Time.fixedDeltaTime, transform.up); // 플레이어의 y축 기준으로 회전
 
     }
+
 }
