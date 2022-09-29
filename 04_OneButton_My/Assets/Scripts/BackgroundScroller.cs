7be6d5f24196c12995d868b4c4a2edf631a80ac6
wiwoiww <wiwoiww2@gmail.com>
2022-09-30 01:32:59 +0900

    9.29예비군수업

--- 04_OneButton_My/Assets/Scripts/BackgroundScroller.cs
+++ 04_OneButton_My/Assets/Scripts/BackgroundScroller.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public float scrollingSpeed = 5.0f;
+    public float width = 7.2f;  // 하늘은 7.2, 바닥은 8.4
 }
