e9b925e98c47c568815af1e6b5f31d973760b6c2
wiwoiww <wiwoiww2@gmail.com>
2022-10-12 04:31:22 +0900

    10.11수업

---
+++ 04_OneButton_My/Assets/Scripts/Obstacles/Spike.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : SpawnBase
+{
+    public Action onDie;
+
+    private void OnDisable()
+    {
+        onDie?.Invoke();
+    }
+}
