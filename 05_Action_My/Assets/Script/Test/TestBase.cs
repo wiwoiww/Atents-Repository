e9641945b544452e9095599d9125428afebac93c
wiwoiww <wiwoiww2@gmail.com>
2022-10-22 03:37:16 +0900

    10.21

--- 05_Action_My/Assets/Script/Test/TestBase.cs
+++ 05_Action_My/Assets/Script/Test/TestBase.cs
@@ -17,18 +17,17 @@ public class TestBase : MonoBehaviour
     {
         inputActions.Test.Enable();
         inputActions.Test.Test1.performed += Test1;
-        inputActions.Test.Test1.performed += Test2;
-        inputActions.Test.Test1.performed += Test3;
-        inputActions.Test.Test1.performed += Test4;
-        inputActions.Test.Test1.performed += Test5;
+        inputActions.Test.Test2.performed += Test2;
+        inputActions.Test.Test3.performed += Test3;
+        inputActions.Test.Test4.performed += Test4;
+        inputActions.Test.Test5.performed += Test5;
     }
-
     protected virtual void OnDisable()
     {
-        inputActions.Test.Test1.performed -= Test5;
-        inputActions.Test.Test1.performed -= Test4;
-        inputActions.Test.Test1.performed -= Test3;
-        inputActions.Test.Test1.performed -= Test2;
+        inputActions.Test.Test5.performed -= Test5;
+        inputActions.Test.Test4.performed -= Test4;
+        inputActions.Test.Test3.performed -= Test3;
+        inputActions.Test.Test2.performed -= Test2;
         inputActions.Test.Test1.performed -= Test1;
         inputActions.Test.Disable();
     }
