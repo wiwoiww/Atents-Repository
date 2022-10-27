4f882b05e599dd3b4ab3f53a64bb05ebe18b5a7b
wiwoiww <wiwoiww2@gmail.com>
2022-10-28 00:59:41 +0900

    10.28수업

--- 05_Action_My/Assets/Script/Item/ItemRotator.cs
+++ 05_Action_My/Assets/Script/Item/ItemRotator.cs
@@ -9,9 +9,26 @@ public class ItemRotator : MonoBehaviour
     public float minHeight = 0.5f;          // 오브젝트의 가장 낮은 높이
     public float maxHeight = 1.5f;          // 오브젝트의 가장 높은 높이
 
+    float timeElapsed;
+    float halfDiff;
+    Vector3 newPosition;
+
+    private void Start()
+    {
+        newPosition = transform.position;
+        newPosition.y = minHeight;
+        transform.position = newPosition;
+        timeElapsed = 0.0f;
+        halfDiff = 0.5f * (maxHeight - minHeight);
+    }
 
     private void Update()
     {
+        timeElapsed += Time.deltaTime;
+        newPosition.y = minHeight + (1 - Mathf.Cos(timeElapsed)) * halfDiff;
+        transform.position = newPosition;
+
         transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+
     }
 }
