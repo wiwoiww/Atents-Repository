32437f46a6d44d01621da86826f9864eeaa1bb0d
wiwoiww <wiwoiww2@gmail.com>
2022-11-17 09:37:12 +0900

    11.17

--- 05_Action_My/Assets/Script/Item/ScripableObject/ItemData_Shield.cs
+++ 05_Action_My/Assets/Script/Item/ScripableObject/ItemData_Shield.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Weapon Item Data", menuName = "Scriptable Object/Item Data - Weapon", order = 5)]
-public class ItemData_Weapon : ItemData_EquipItem
+[CreateAssetMenu(fileName = "New shield Item Data", menuName = "Scriptable Object/Item Data - shield", order = 6)]
+public class ItemData_Shield : ItemData_EquipItem
 {
     [Header("무기 데이터")]
-    public float attackPower = 30;
+    public float defencePower = 15;
 
-    public override EquipPartType EquipPart => EquipPartType.Weapon;
+    public override EquipPartType EquipPart => EquipPartType.Shield;
 
     //public override void EquipItem(GameObject target)
     //{
-    //}
+    //} 
 
     //public override void ToggleEquipItem(GameObject target)
     //{
