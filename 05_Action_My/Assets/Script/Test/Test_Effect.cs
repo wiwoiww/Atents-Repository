637b1f7b0de8636d8ddbab980ebd9fdb68fd9c4e
wiwoiww <wiwoiww2@gmail.com>
2022-11-22 18:19:56 +0900

    11.22이론

---
+++ 05_Action_My/Assets/Script/Test/Test_Effect.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_Effect : TestBase
+{
+    TrailRenderer trail;
+    LineRenderer line;
+
+    private void Start()
+    {
+        trail = FindObjectOfType<TrailRenderer>();
+        line = FindObjectOfType<LineRenderer>();
+        line.startColor = Color.green;
+    }
+
+    protected override void Test1(InputAction.CallbackContext _)
+    {
+        trail.time = 0.1f;
+    }  
+    
+    protected override void Test2(InputAction.CallbackContext _)
+    {
+        trail.time = 3.0f;
+    }
+
+    protected override void Test3(InputAction.CallbackContext _)
+    {
+        trail.startWidth = 0.0f;
+    }
+
+    protected override void Test4(InputAction.CallbackContext _)
+    {
+        trail.startWidth = 2.0f;
+    }
+}
