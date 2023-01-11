8fd64335bb50c5590504ddb1242d08945b5c4498
wiwoiww <wiwoiww2@gmail.com>
2023-01-11 12:45:40 +0900

    23.01.10

--- 07_TileMap_MY/Assets/Scripts/Manager/GameManager.cs
+++ 07_TileMap_MY/Assets/Scripts/Manager/GameManager.cs
@@ -14,20 +14,21 @@ public class GameManager : Singleton<GameManager>
 
     public MapManager MapManager => mapManager;
 
-    protected override void ResetData()
-    {
-        base.ResetData();
-        player = FindObjectOfType<Player>();
-    }
-
     protected override void Initialize()
     {
         base.Initialize();
 
         mapManager = GetComponent<MapManager>();    // 맵 매니저 찾아서
-        if (mapManager != null)
-        {
-            mapManager.Initialize();                // 초기화 하기
-        }
+        mapManager.Initialize();                    // 초기화 하기
+    }
+
+    /// <summary>
+    /// 씬이 로드 될 때마다 반복적으로 다시 설정해야 할 것들
+    /// </summary>
+    protected override void ManagerDataReset()
+    {
+        base.ManagerDataReset();
+        player = FindObjectOfType<Player>();    // 씬을 나가면 플레이어가 사라지니 다시 찾기
+        mapManager.MapDataReset();              // 데이터 초기화 및 플레이어 주변맵 로딩하기
     }
 }
